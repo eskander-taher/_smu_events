23432b72b428a078cbac6864b46fc8fed12e86eb
eskander-taher <eskander.alshaibany@gmail.com>
2024-06-26 23:01:36 +0500

    add build files

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const env = require("dotenv").config();
 const colors = require("colors");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
  
 // Express app initialization
 const app = express();
